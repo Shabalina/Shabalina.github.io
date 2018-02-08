b82dde08a35dc4cb029741c56a1b131eb20e88c2
Anastasia Shapovalova <shabalinastya@gmail.com>
2018-02-08 13:54:54 +0000

    edit ops

--- Mr_Burger/js/site_navigation.js
+++ Mr_Burger/js/site_navigation.js
@@ -77,7 +77,10 @@ $(document).ready(function() {
   var  display = $('.maincontent');
     inScroll = false;
 
- var md = new MobileDetect(window.navigator.userAgent);
+ var md = new MobileDetect(
+  'Mozilla/5.0 (Linux; U; Android 4.0.3; en-in; SonyEricssonMT11i' +
+  ' Build/4.1.A.0.562) AppleWebKit/534.30 (KHTML, like Gecko)' +
+  ' Version/4.0 Mobile Safari/534.30');
     isMobile = md.mobile();
 
   var performTransition = function (sectionEq) {
