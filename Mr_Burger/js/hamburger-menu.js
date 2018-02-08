83d3fdd7e2fd868a047b9798b55b58c0eb9747d3
Anastasia Shapovalova <shabalinastya@gmail.com>
2018-02-08 12:51:36 +0000

    edit phone menu

--- Mr_Burger/js/hamburger-menu.js
+++ Mr_Burger/js/hamburger-menu.js
@@ -12,6 +12,13 @@ $(document).ready(function() {
     $('.hamburger-menu').fadeOut(800);
 
   });
+
+  $('.nav__link-hamburger').on('click', function(e) {
+    e.preventDefault()
+
+    $('.hamburger-menu').fadeOut(800);
+
+  });
 })
 
 $(document).ready(function() {
