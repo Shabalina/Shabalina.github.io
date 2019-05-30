8044946a3538743b8b205c188e28b8c2b62bd5ee
Anastasia Shapovalova <shabalinastya@gmail.com>
2019-05-30 16:04:29 +0100

    build for pokemon-app

---
+++ pokemon_app/build/precache-manifest.10bd510cc48f4a6feaae43ca3de7230a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8de3a7894c2a1fb9d7e8",
+    "url": "/static/css/main.c2db092d.chunk.css"
+  },
+  {
+    "revision": "8de3a7894c2a1fb9d7e8",
+    "url": "/static/js/main.8de3a789.chunk.js"
+  },
+  {
+    "revision": "918c384a10c9bd86ba8e",
+    "url": "/static/js/1.918c384a.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "17ce4edfcd87cf938461f06d160a3c3a",
+    "url": "/index.html"
+  }
+];
